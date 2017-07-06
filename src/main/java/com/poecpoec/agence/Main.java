3b9d184e67966a53a9c9c808f75954a9345ee369
rplantefeve <romain.plantefeve@gmail.com>
2017-07-06 09:29:45 +0200

    Création d'une nouvelle classe
    Affichage d'un texte

--- src/main/java/com/poecpoec/agence/Main.java
+++ src/main/java/com/poecpoec/agence/Main.java
@@ -3,6 +3,8 @@
  */
 package com.poecpoec.agence;
 
+
+
 /**
  * @author Seme
  *
@@ -15,6 +17,8 @@ public class Main
      */
     public static void main(String[] args) {
         System.out.println("Hello Poec Poec !");
+        Test test = new Test();
+        test.afficher();
     }
 
 }
