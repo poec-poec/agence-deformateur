c8f2e73ca2b0e15dd776e8abcdca5bd1520004f5
rplantefeve <romain.plantefeve@gmail.com>
2017-07-07 10:20:58 +0200

    Gestion des exceptions

--- src/main/java/com/poecpoec/agence/Main.java
+++ src/main/java/com/poecpoec/agence/Main.java
@@ -13,17 +13,16 @@ import java.util.List;
 
 /**
  * @author Seme
- *
  */
 public class Main
 {
 
     /**
+     * 
      * @param args
-     * @throws ClassNotFoundException 
-     * @throws SQLException 
      */
-    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+    public static void main(String[] args)
+    {
         /*
          * Tests unitaires itération #1
          */
@@ -33,7 +32,7 @@ public class Main
         Ville londres = new Ville("Londres", "UK");
         System.out.println(cdg);
         System.out.println(paris);
-        
+
         /*
          * UT it#2
          */
@@ -41,46 +40,61 @@ public class Main
         cdg.ajouterVille(paris);
         cdg.ajouterVille(londres);
         System.out.println(cdg);
-        
+
         /*
          * UT it#3
          */
         System.out.println("Test it#3");
         System.out.println(paris);
-        
+
         /*
          * UT it#4 : test de la connexion à la BDD
          */
         // déclaration des variables
         List<Aeroport> aeroports = new ArrayList<>();
-        
-        // Etape 1 : chargement du driver
-        Class.forName("com.mysql.jdbc.Driver");
-        // Etape 2 : création de la connexion
-        String dsn = "jdbc:mysql://localhost:3306/agence";
-        Connection connexion = DriverManager.getConnection(dsn, "user", "password");
-        // Etape 3 : création du statement
-        Statement statement = connexion.createStatement();
-        // Etape 4 : Exécuter la requête SQL
-        ResultSet resultats = statement.executeQuery("SELECT * FROM aeroport");
-        // Etape 5 : boucle de parcours des résultats
-        while(resultats.next()) {
-            // je crée un aéroport vide
-            Aeroport aeroport = new Aeroport();
-            aeroport.setNom(resultats.getString("nom"));
-            // je l'ajoute à ma liste
-            aeroports.add(aeroport);
+
+        try
+        {
+            // Etape 1 : chargement du driver
+            Class.forName("com.mysql.jdbc.Driver");
+            // Etape 2 : création de la connexion
+            String dsn = "jdbc:mysql://localhost:3306/agence";
+            Connection connexion = DriverManager.getConnection(dsn, "user",
+                    "password");
+            // Etape 3 : création du statement
+            Statement statement = connexion.createStatement();
+            // Etape 4 : Exécuter la requête SQL
+            ResultSet resultats = statement
+                    .executeQuery("SELECT * FROM aeroport");
+            // Etape 5 : boucle de parcours des résultats
+            while (resultats.next())
+            {
+                // je crée un aéroport vide
+                Aeroport aeroport = new Aeroport();
+                aeroport.setNom(resultats.getString("nom"));
+                // je l'ajoute à ma liste
+                aeroports.add(aeroport);
+            }
+            // Etape 6 : fermer le résultat
+            resultats.close();
+            // Etape 7 : fermer le statement
+            statement.close();
+            // Etape 8 : fermer la connexion
+            connexion.close();
         }
-        // Etape 6 : fermer le résultat
-        resultats.close();
-        // Etape 7 : fermer le statement
-        statement.close();
-        // Etape 8 : fermer la connexion
-        connexion.close();
-        
+        catch (ClassNotFoundException e)
+        {
+            System.out.println("Impossible de charger le driver. Vérifier votre classpath.");
+        }
+        catch (SQLException e)
+        {
+            System.out.println("Erreur SQL. Voir ci-après.");
+            System.out.println(e.getMessage());
+        }
+
         // j'affiche mes aéroports
         System.out.println(aeroports);
-        
+
     }
 
 }
