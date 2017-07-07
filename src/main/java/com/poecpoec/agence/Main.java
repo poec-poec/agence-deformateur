46806fda65e8e5789d2bded50b58669f9d84ac0d
rplantefeve <romain.plantefeve@gmail.com>
2017-07-07 09:11:29 +0200

    UT connexion successful

--- src/main/java/com/poecpoec/agence/Main.java
+++ src/main/java/com/poecpoec/agence/Main.java
@@ -5,7 +5,11 @@ package com.poecpoec.agence;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author Seme
@@ -47,11 +51,35 @@ public class Main
         /*
          * UT it#4 : test de la connexion à la BDD
          */
+        // déclaration des variables
+        List<Aeroport> aeroports = new ArrayList<>();
+        
         // Etape 1 : chargement du driver
         Class.forName("com.mysql.jdbc.Driver");
         // Etape 2 : création de la connexion
         String dsn = "jdbc:mysql://localhost:3306/agence";
         Connection connexion = DriverManager.getConnection(dsn, "user", "password");
+        // Etape 3 : création du statement
+        Statement statement = connexion.createStatement();
+        // Etape 4 : Exécuter la requête SQL
+        ResultSet resultats = statement.executeQuery("SELECT * FROM aeroport");
+        // Etape 5 : boucle de parcours des résultats
+        while(resultats.next()) {
+            // je crée un aéroport vide
+            Aeroport aeroport = new Aeroport();
+            aeroport.setNom(resultats.getString("nom"));
+            // je l'ajoute à ma liste
+            aeroports.add(aeroport);
+        }
+        // Etape 6 : fermer le résultat
+        resultats.close();
+        // Etape 7 : fermer le statement
+        statement.close();
+        // Etape 8 : fermer la connexion
+        connexion.close();
+        
+        // j'affiche mes aéroports
+        System.out.println(aeroports);
         
     }
 
