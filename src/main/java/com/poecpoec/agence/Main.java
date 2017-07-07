3ad32449dbf825664db0848bc3722652815d2e46
rplantefeve <romain.plantefeve@gmail.com>
2017-07-07 14:22:31 +0200

    Refactoring DAO + interface

--- src/main/java/com/poecpoec/agence/Main.java
+++ src/main/java/com/poecpoec/agence/Main.java
@@ -18,7 +18,6 @@ public class Main
 {
 
     /**
-     * 
      * @param args
      */
     public static void main(String[] args)
@@ -53,44 +52,13 @@ public class Main
         // déclaration des variables
         List<Aeroport> aeroports = new ArrayList<>();
 
-        try
-        {
-            // Etape 1 : chargement du driver
-            Class.forName("com.mysql.jdbc.Driver");
-            // Etape 2 : création de la connexion
-            String dsn = "jdbc:mysql://localhost:3306/agence";
-            Connection connexion = DriverManager.getConnection(dsn, "user",
-                    "password");
-            // Etape 3 : création du statement
-            Statement statement = connexion.createStatement();
-            // Etape 4 : Exécuter la requête SQL
-            ResultSet resultats = statement
-                    .executeQuery("SELECT * FROM aeroport");
-            // Etape 5 : boucle de parcours des résultats
-            while (resultats.next())
-            {
-                // je crée un aéroport vide
-                Aeroport aeroport = new Aeroport();
-                aeroport.setNom(resultats.getString("nom"));
-                // je l'ajoute à ma liste
-                aeroports.add(aeroport);
-            }
-            // Etape 6 : fermer le résultat
-            resultats.close();
-            // Etape 7 : fermer le statement
-            statement.close();
-            // Etape 8 : fermer la connexion
-            connexion.close();
-        }
-        catch (ClassNotFoundException e)
-        {
-            System.out.println("Impossible de charger le driver. Vérifier votre classpath.");
-        }
-        catch (SQLException e)
-        {
-            System.out.println("Erreur SQL. Voir ci-après.");
-            System.out.println(e.getMessage());
-        }
+
+        // j'ai besoin du DAO, alors je l'instancie
+        AeroportDao aeroportDao = new AeroportDao();
+        // j'appelle la méthode pour récupérer tous les objets de classe
+        // Aéroport de la BDD à l'aide du DAO Aéroport
+        aeroports = aeroportDao.findAll();
+        
 
         // j'affiche mes aéroports
         System.out.println(aeroports);
