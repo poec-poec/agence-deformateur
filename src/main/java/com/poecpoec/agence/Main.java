7d647c4d99144de82573cd0af91329af323cdbc8
rplantefeve <romain.plantefeve@gmail.com>
2017-07-12 14:18:43 +0200

    New UT

--- src/main/java/com/poecpoec/agence/Main.java
+++ src/main/java/com/poecpoec/agence/Main.java
@@ -6,9 +6,11 @@ package com.poecpoec.agence;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.poecpoec.agence.dao.AdresseDao;
 import com.poecpoec.agence.dao.AeroportDao;
 import com.poecpoec.agence.dao.ClientDao;
 import com.poecpoec.agence.dao.VilleDao;
+import com.poecpoec.agence.model.Adresse;
 import com.poecpoec.agence.model.Aeroport;
 import com.poecpoec.agence.model.Client;
 import com.poecpoec.agence.model.Ville;
@@ -18,11 +20,58 @@ import com.poecpoec.agence.model.Ville;
  */
 public class Main
 {
-
     /**
      * @param args
      */
     public static void main(String[] args)
+    {
+        // testNonRegression();
+        //
+        // // TEAM MMA
+        // System.out.println("Test MMA");
+        // // IT#3 Création client DAO.
+        // List<Client> clients = new ArrayList<>();
+        // ClientDao clientDao = new ClientDao();
+        // clients = clientDao.findAll();
+        // System.out.println(clients);
+
+        // TU#1 tests Adresse
+        System.out.println("--------------------------------------------");
+        System.out.println("TU#1 - Adresses seules (findAll et findById)");
+        System.out.println("--------------------------------------------");
+        System.out.println("Liste adresses :\n");
+        AdresseDao adresseDao = new AdresseDao();
+        List<Adresse> adresses = adresseDao.findAll();
+        System.out.println(adresses);
+        System.out.println("---------");
+
+        System.out.println("2 cas d'adresses seules par leur ID :\n");
+        Adresse adrInconnue = adresseDao.findById(8);
+        System.out.println(adrInconnue);
+        Adresse adrConnue = adresseDao.findById(1);
+        System.out.println(adrConnue);
+        System.out.println("---------");
+
+        // TU#2 tests Client
+        System.out.println("-----------------------------------------------");
+        System.out.println("TU#1 - Clients + Adresses (findAll et findById)");
+        System.out.println("-----------------------------------------------");
+        System.out.println("Liste Clients avec leur adresse :\n");
+        ClientDao clientDao = new ClientDao();
+        List<Client> clients = clientDao.findAll();
+        System.out.println(clients);
+        System.out.println("---------");
+
+        System.out.println("2 cas de clients seuls avec leur adresse par leur ID :\n");
+        Client clientInconnu = clientDao.findById(8);
+        System.out.println(clientInconnu);
+        Client clientConnu = clientDao.findById(1);
+        System.out.println(clientConnu);
+        System.out.println("---------");
+
+    }
+
+    public static void testNonRegression()
     {
         /*
          * Tests unitaires itération #1
@@ -95,15 +144,6 @@ public class Main
         // toutes les villes
         List<Ville> villes = villeDao.findAll();
         System.out.println(villes);
-
-        // TEAM MMA
-        System.out.println("Test MMA");
-        // IT#3 Création client DAO.
-        List<Client> clients = new ArrayList<>();
-        ClientDao clientDao = new ClientDao();
-        clients = clientDao.findAll();
-        System.out.println(clients);
-
     }
 
 }
